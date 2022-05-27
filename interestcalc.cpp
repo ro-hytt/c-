5489179c679d55f0a55432556ba4ffdb92226078
R O H Y T T <60282545+ro-hytt@users.noreply.github.com>
2022-05-27 12:40:26 +0530

    Add files via upload

---
+++ interestcalc.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<iostream>
+void main()
+{
+    
+float a,r,t,i;
+cout<<"insert amount";
+cin>>a;
+cout>>"enter rate";
+cin>>r;
+cout>>"enter time";
+cin>>t;
+i= (a*r*t)/100;
+cout<<"interest is="<<i;
+
+}
